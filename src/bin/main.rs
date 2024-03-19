e66239121b14f294d480afdd397b92caf4100c97
Nabin Thapa <erewhonaden23@gmail.com>
2024-03-19 14:09:18 +0545

    Multi threaded HTTP server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,19 @@
+use server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
@@ -20,6 +24,7 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
@@ -31,6 +36,9 @@ fn handle_connection(mut stream: TcpStream) {
     //
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 Ok", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 Ok", "index.html")
     } else {
         ("HTTP/1.1 404 Not Found", "404.html")
