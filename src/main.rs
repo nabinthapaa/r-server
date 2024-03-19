64ba9a0e9dbb16ad4121fe0a0d96457700afdc2f
Nabin Thapa <erewhonaden23@gmail.com>
2024-03-19 13:33:03 +0545

    spawing threads to every request

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::fmt::format;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream)
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
